9dfb4f92017a7be3f4968e5c9943a165086dbb92
Arunav Dey <arunav.dey@innovaccer.com>
2021-12-07 17:05:04 +0530

    WIP: Define structure for frontend part

---
+++ ecommerce-site-frontend/src/components/homepage/Homepage.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Homepage = () => {
+
+render()
+{
+    return(
+        <>
+            <div>Homepage</div>
+        </>
+    )
+}
+
+}
+export default Homepage;
---
+++ ecommerce-site-frontend/src/components/main/Main.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Main = () => {
+
+render()
+{
+    return(
+        <>
+            <div>Main Page</div>
+        </>
+    )
+}
+
+}
+export default Main;
