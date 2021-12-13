8da4a1f9c87d3253ba0a885cb32ac8620d8a05c7
Arunav Dey <arunav.dey@innovaccer.com>
2021-12-13 19:39:50 +0530

    BUG FIX: Resolved Product Description Page not loading data error.

--- ecommerce-site-frontend/src/pages/pdp/Pdp.js
+++ ecommerce-site-frontend/src/pages/pdp/Pdp.js
@@ -1,26 +1,40 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+import React, {useState ,useEffect} from 'react';
 import PageHeading from '../../components/pageheading/PageHeading';
 import Pcontainer from '../../components/pcontainer/Pcontainer';
 import '../../pages/pdp/styles.css'
 import Navbar from '../../components/navbar/Navbar'
 import Pcard from '../../components/pcard/Pcard';
-import {getProductForId} from '../../utils/Product'
+//import {getProductForId} from '../../utils/Product'
+
+const getProductForId = (product_id) =>{
+    var receivedProduct = {}
+    fetch('/api/getproductById/'+product_id)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+    return receivedProduct;
+}
 
 const Pdp = (props) =>{
 
+    const [pInfo, setPInfo] = useState([])
     let params = (new URL(document.location)).searchParams;
     let product_id = params.get('product_id');
-    const product = getProductForId(product_id);
-    console.info('PRPRPRP ',product)
+    
+    useEffect(() => {
+        fetch('/api/getproductById/'+product_id)
+            .then((res) => res.json())
+            .then((res) => setPInfo(res))
+    }, [])
+
     render()
-    {
+    {   console.info('PINFO',pInfo[0])
         return(
             <>
-                <PageHeading content={product.pname}/>
+                <PageHeading content={"Product Info:"}/>
                 <div className="outer_container">
                     <div className="inner_container">
-                        <Pcard {...product}/>
+                        <Pcard {...pInfo[0]}/>
                     </div>
                 </div>
             </>
